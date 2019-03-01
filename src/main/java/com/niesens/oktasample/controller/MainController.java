1a0c65ed440d29e634f2f89588ec6f74ca14b95f
Claus Niesen <cniesen@users.noreply.github.com>
2019-03-01 17:05:40 -0600

    Simplified getting the preferred username.  We got rid of a utility class but now rely on OIDC in our code.

--- src/main/java/com/niesens/oktasample/controller/MainController.java
+++ src/main/java/com/niesens/oktasample/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.niesens.oktasample.controller;
 
 import com.niesens.oktasample.config.RoleConfig;
-import com.niesens.oktasample.utils.PrincipalUtils;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -18,23 +17,23 @@ import java.security.Principal;
 public class MainController {
 
     @RequestMapping("/")
-    public String index(Principal principal) {
-        if (principal == null) {
+    public String index(@AuthenticationPrincipal OidcUser user) {
+        if (user == null) {
             return "Public - not authenticated";
         } else {
-            return "Public - authenticated as " + PrincipalUtils.getPreferredUsername(principal);
+            return "Public - authenticated as " + user.getAttributes().get("preferred_username");
         }
     }
 
     @RequestMapping("/user")
     @PreAuthorize("hasAuthority(@roleConfig.getUserRoleName())")
-    public String user(Principal principal) {
-        return "User - authenticated as " + PrincipalUtils.getPreferredUsername(principal);
+    public String user(@AuthenticationPrincipal OidcUser user) {
+        return "User - authenticated as " + user.getAttributes().get("preferred_username");
     }
 
     @RequestMapping("/admin")
-    public String admin(Principal principal) {
-        return "Administrator - authenticated as " + PrincipalUtils.getPreferredUsername(principal);
+    public String admin(@AuthenticationPrincipal OidcUser user) {
+        return "Administrator - authenticated as " + user.getAttributes().get("preferred_username");
     }
 
 }
